8b779dba0d1be4ccc73e3b3a9a033a43247ad2d7
Rawlings <hfang@91souban.com>
2016-12-20 18:09:01 +0800

    ci

--- Sources/PagerTabStripViewController.swift
+++ Sources/PagerTabStripViewController.swift
@@ -142,7 +142,7 @@ open class PagerTabStripViewController: UIViewController, UIScrollViewDelegate {
             containerView.setContentOffset(CGPoint(x: pageOffsetForChild(at: index), y: 0), animated: true)
         }
         else {
-            (navigationController?.view ?? view).isUserInteractionEnabled = !animated
+//            (navigationController?.view ?? view).isUserInteractionEnabled = !animated
             containerView.setContentOffset(CGPoint(x: pageOffsetForChild(at: index), y: 0), animated: animated)
         }
     }
